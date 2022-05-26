3d967f058c5f30de33078d5d3eb3cd071bc09c82
jnpdx <john@johnnastos.com>
2022-05-25 23:06:14 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by John Nastos on 5/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RemotionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemotionSwiftUIApp.swift
+//  Shared
+//
+//  Created by John Nastos on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RemotionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
